277c22ac05425a467a9bfcc70e110c1748393fca
xieyiwen <wsxieyiwen@outlook.com>
2019-07-05 00:28:50 +0800

    delete log

--- okex-go-sdk-api/client.go
+++ okex-go-sdk-api/client.go
@@ -138,13 +138,11 @@ func (client *Client) Request(method string, requestPath string,
 		return response, nil
 	} else if status >= 400 || status <= 500 {
 		errMsg := "Http error(400~500) result: status=" + IntToString(status) + ", message=" + message + ", body=" + responseBodyString
-		fmt.Println(errMsg)
 		if body != nil {
 			err := errors.New(errMsg)
 			return response, err
 		}
 	} else {
-		fmt.Println("Http error result: status=" + IntToString(status) + ", message=" + message + ", body=" + responseBodyString)
 		return response, errors.New(message)
 	}
 	return response, nil
